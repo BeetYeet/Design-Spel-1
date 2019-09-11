e5d1b19b8e69ced48119ec9ed3f8d9d415a9c416
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2019-09-11 10:00:02 +0200

    no message

--- Assets/Script/UIController.cs
+++ Assets/Script/UIController.cs
@@ -1,11 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class UImanager : MonoBehaviour
+public class UIController : MonoBehaviour
 {
-   
     // Start is called before the first frame update
     void Start()
     {
