897906120c2fae1826e13f2c70ffb7c0282c1ed4
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-09-25 09:26:43 +0200

    spitballs and less jump bounce

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -6,327 +6,328 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class PlayerMovement : MonoBehaviour
 {
-    #region vars
-    public MovementType movementType;
-    [Space]
-    public MovementState movementState;
-    public MovingDirection movingDirection;
-    [Space]
-    public PlatformVars platformVaribles;
-    public TopDownVars TopdownVaribles;
+	#region vars
+	public MovementType movementType;
+	[Space]
+	public MovementState movementState;
+	public MovingDirection movingDirection;
+	[Space]
+	public PlatformVars platformVaribles;
+	public TopDownVars TopdownVaribles;
 
-    Rigidbody2D rbody;
-    AudioSource AudioData;
-    SpriteRenderer SR;
+	Rigidbody2D rbody;
+	AudioSource AudioData;
+	SpriteRenderer SR;
 
-    public static PlayerMovement i
-    {
-        get; private set;
-    }
-    #endregion
+	public static PlayerMovement i
+	{
+		get; private set;
+	}
+	#endregion
 
-    void OnValidate()
-    {
-        rbody = GetComponent<Rigidbody2D>();
-        AudioData = GetComponent<AudioSource>();
-        SR = GetComponent<SpriteRenderer>();
-        switch (movementType)
-        {
-            case MovementType.Platformer:
-                if (platformVaribles.UseDoubleJump == false)
-                {
-                    platformVaribles.doubleJumps = 0;
-                }
-                if (platformVaribles.UseJumpRotation == false)
-                {
-                    platformVaribles.JumpRotation = 0f;
-                    platformVaribles.JumpRotationMuliplyer = 0f;
-                }
-                if (platformVaribles.UseCustomGravity == false)
-                {
-                    rbody.gravityScale = 5f;
-                }
-                break;
+	void OnValidate()
+	{
+		rbody = GetComponent<Rigidbody2D>();
+		AudioData = GetComponent<AudioSource>();
+		SR = GetComponent<SpriteRenderer>();
+		switch (movementType)
+		{
+			case MovementType.Platformer:
+				if (platformVaribles.UseDoubleJump == false)
+				{
+					platformVaribles.doubleJumps = 0;
+				}
+				if (platformVaribles.UseJumpRotation == false)
+				{
+					platformVaribles.JumpRotation = 0f;
+					platformVaribles.JumpRotationMuliplyer = 0f;
+				}
+				if (platformVaribles.UseCustomGravity == false)
+				{
+					rbody.gravityScale = 5f;
+				}
+				break;
 
-            case MovementType.TopDown:
-                if (TopdownVaribles.SameSpeedOnXAndY == true)
-                {
-                    TopdownVaribles.YSpeed = TopdownVaribles.XSpeed;
-                }
-                break;
-        }
-    }
+			case MovementType.TopDown:
+				if (TopdownVaribles.SameSpeedOnXAndY == true)
+				{
+					TopdownVaribles.YSpeed = TopdownVaribles.XSpeed;
+				}
+				break;
+		}
+	}
 
-    void Start()
-    {
-        i = this;
-        switch (movementType)
-        {
-            case MovementType.Platformer:
-                rbody.bodyType = RigidbodyType2D.Dynamic;
+	void Start()
+	{
+		i = this;
+		switch (movementType)
+		{
+			case MovementType.Platformer:
+				rbody.bodyType = RigidbodyType2D.Dynamic;
 
-                break;
-            case MovementType.TopDown:
-                rbody.bodyType = RigidbodyType2D.Kinematic;
-                break;
-        }
-    }
+				break;
+			case MovementType.TopDown:
+				rbody.bodyType = RigidbodyType2D.Kinematic;
+				break;
+		}
+	}
 
-    void Update()
-    {
-        Enums();
-        switch (movementType)
-        {
-            case MovementType.Platformer:
-                Movement();
-                break;
-            case MovementType.TopDown:
-                TopDownMovement();
-                break;
-        }
-    }
+	void Update()
+	{
+		Enums();
+		switch (movementType)
+		{
+			case MovementType.Platformer:
+				Movement();
+				break;
+			case MovementType.TopDown:
+				TopDownMovement();
+				break;
+		}
+	}
 
-    #region PlatformStuff
+	#region PlatformStuff
 
-    void Movement()
-    {
-        rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * platformVaribles.Speed, rbody.velocity.y);
-        switch (movementState)
-        {
-            case MovementState.Moving:
-                AudioData.PlayOneShot(platformVaribles.walkingSound);
-                break;
-        }
-        switch (movingDirection)
-        {
-            case MovingDirection.Left:
-                if (platformVaribles.FlipSpriteOnMovement == true)
-                    SR.flipX = true;
-                break;
-            case MovingDirection.Right:
-                if (platformVaribles.FlipSpriteOnMovement == true)
-                    SR.flipX = false;
-                break;
-        }
-        Jump();
+	void Movement()
+	{
+		rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * platformVaribles.Speed, rbody.velocity.y);
+		switch (movementState)
+		{
+			case MovementState.Moving:
+				AudioData.PlayOneShot(platformVaribles.walkingSound);
+				break;
+		}
+		switch (movingDirection)
+		{
+			case MovingDirection.Left:
+				if (platformVaribles.FlipSpriteOnMovement == true)
+					SR.flipX = true;
+				break;
+			case MovingDirection.Right:
+				if (platformVaribles.FlipSpriteOnMovement == true)
+					SR.flipX = false;
+				break;
+		}
+		Jump();
 
-    }
+	}
 
-    void Jump()
-    {
-        Vector2 jumpForce = new Vector2(rbody.velocity.x, platformVaribles.JumpForce);
+	void Jump()
+	{
+		Vector2 jumpForce = new Vector2(rbody.velocity.x, platformVaribles.JumpForce);
 
-        switch (platformVaribles.groundState)
-        {
-            case GroundState.onGround:
-                platformVaribles.Jumps = platformVaribles.doubleJumps;
-                if (Input.GetButtonDown("Jump"))
-                {
-                    rbody.velocity = jumpForce;
-                    switch (movingDirection)
-                    {
+		switch (platformVaribles.groundState)
+		{
+			case GroundState.onGround:
+				platformVaribles.Jumps = platformVaribles.doubleJumps;
+				if (Input.GetButtonDown("Jump"))
+				{
+					rbody.velocity = jumpForce;
+					switch (movingDirection)
+					{
 
-                        case MovingDirection.Right:
-                            rbody.AddTorque(-platformVaribles.JumpRotation);
-                            break;
-                        case MovingDirection.Left:
-                            rbody.AddTorque(platformVaribles.JumpRotation);
-                            break;
-                    }
-                    AudioData.PlayOneShot(platformVaribles.JumpSound);
-                }
-                #region SnapRot
-                if (platformVaribles.snaprotation == true)
-                {
-                    Quaternion Target = Quaternion.Euler(0, 0, 0);
+						case MovingDirection.Right:
+							rbody.AddTorque(-platformVaribles.JumpRotation);
+							break;
+						case MovingDirection.Left:
+							rbody.AddTorque(platformVaribles.JumpRotation);
+							break;
+					}
+					AudioData.PlayOneShot(platformVaribles.JumpSound);
+				}
+				#region SnapRot
+				if (platformVaribles.snaprotation == true)
+				{
+					Quaternion Target = Quaternion.Euler(0, 0, 0);
 
-                    if (transform.rotation.z == 0)
-                        return;
-                    else
-                        transform.rotation = Quaternion.Slerp(transform.rotation, Target, Time.deltaTime * platformVaribles.SmoothRotation);
-                }
-                SR.sprite = platformVaribles.WalkingSprite;
-                #endregion
-                break;
-
-
-            case GroundState.InAir:
-                if (Input.GetButtonDown("Jump") && platformVaribles.Jumps != 0)
-                {
-                    rbody.velocity = jumpForce / 1.1f;
-                    switch (movingDirection)
-                    {
-                        case MovingDirection.Right:
-                            rbody.AddTorque(platformVaribles.JumpRotation * -platformVaribles.JumpRotationMuliplyer);
-                            break;
-                        case MovingDirection.Left:
-                            rbody.AddTorque(platformVaribles.JumpRotation * platformVaribles.JumpRotationMuliplyer);
-                            break;
-                    }
-                    AudioData.PlayOneShot(platformVaribles.JumpSound);
-                    platformVaribles.Jumps--;
-                }
-                SR.sprite = platformVaribles.JumpSprite;
-                break;
-        }
-
-        platformVaribles.AirJumpsLeft = platformVaribles.Jumps;
-    }
+					if (transform.rotation.z == 0)
+						return;
+					else
+						transform.rotation = Quaternion.Slerp(transform.rotation, Target, Time.deltaTime * platformVaribles.SmoothRotation);
+				}
+				SR.sprite = platformVaribles.WalkingSprite;
+				#endregion
+				break;
 
 
-    #region OnTrigger
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == platformVaribles.groundTag)
-        {
-            platformVaribles.groundState = GroundState.onGround;
-        }
-    }
+			case GroundState.InAir:
+				if (Input.GetButtonDown("Jump") && platformVaribles.Jumps != 0)
+				{
+					rbody.velocity = jumpForce / 1.1f;
+					switch (movingDirection)
+					{
+						case MovingDirection.Right:
+							rbody.AddTorque(platformVaribles.JumpRotation * -platformVaribles.JumpRotationMuliplyer);
+							break;
+						case MovingDirection.Left:
+							rbody.AddTorque(platformVaribles.JumpRotation * platformVaribles.JumpRotationMuliplyer);
+							break;
+					}
+					AudioData.PlayOneShot(platformVaribles.JumpSound);
+					platformVaribles.Jumps--;
+				}
+				SR.sprite = platformVaribles.JumpSprite;
+				break;
+		}
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == platformVaribles.groundTag)
-        {
+		platformVaribles.AirJumpsLeft = platformVaribles.Jumps;
+	}
 
-            platformVaribles.groundState = GroundState.InAir;
-        }
-    }
-    #endregion OnTrigger
 
-    #endregion
+	#region OnTrigger
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == platformVaribles.groundTag)
+		{
+			platformVaribles.groundState = GroundState.onGround;
+			rbody.velocity /= 5;
+		}
+	}
 
-    #region TopdownStuff
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.tag == platformVaribles.groundTag)
+		{
 
-    void TopDownMovement()
-    {
-        if (TopdownVaribles.SmoothMovement == true)
-            rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * TopdownVaribles.XSpeed, Input.GetAxis("Vertical") * TopdownVaribles.YSpeed);
-        if (TopdownVaribles.SmoothMovement == false)
-            rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * TopdownVaribles.XSpeed, Input.GetAxisRaw("Vertical") * TopdownVaribles.YSpeed);
+			platformVaribles.groundState = GroundState.InAir;
+		}
+	}
+	#endregion OnTrigger
 
-    }
+	#endregion
 
-    #endregion
+	#region TopdownStuff
 
-    void Enums()
-    {
-        if ((rbody.velocity.x < -0.1 || rbody.velocity.x > 0.1) && platformVaribles.groundState == GroundState.onGround)
-            movementState = MovementState.Moving;
-        else
-            movementState = MovementState.Idle;
+	void TopDownMovement()
+	{
+		if (TopdownVaribles.SmoothMovement == true)
+			rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * TopdownVaribles.XSpeed, Input.GetAxis("Vertical") * TopdownVaribles.YSpeed);
+		if (TopdownVaribles.SmoothMovement == false)
+			rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * TopdownVaribles.XSpeed, Input.GetAxisRaw("Vertical") * TopdownVaribles.YSpeed);
 
-        if (rbody.velocity.x < -0.1)
-        {
-            movingDirection = MovingDirection.Left;
-        }
-        if (rbody.velocity.x > 0.1)
-            movingDirection = MovingDirection.Right;
-        switch (movementType)
-        {
-            case MovementType.TopDown:
+	}
 
-                if (rbody.velocity.y < 0.1f)
-                {
-                    movingDirection = MovingDirection.down;
-                }
-                if (rbody.velocity.y > -0.1f)
-                    movingDirection = MovingDirection.up;
+	#endregion
 
-                break;
-        }
+	void Enums()
+	{
+		if ((rbody.velocity.x < -0.1 || rbody.velocity.x > 0.1) && platformVaribles.groundState == GroundState.onGround)
+			movementState = MovementState.Moving;
+		else
+			movementState = MovementState.Idle;
 
-        switch (platformVaribles.groundState)
-        {
-            case GroundState.InAir:
-                movementState = MovementState.Jumping;
-                break;
-        }
-    }
+		if (rbody.velocity.x < -0.1)
+		{
+			movingDirection = MovingDirection.Left;
+		}
+		if (rbody.velocity.x > 0.1)
+			movingDirection = MovingDirection.Right;
+		switch (movementType)
+		{
+			case MovementType.TopDown:
+
+				if (rbody.velocity.y < 0.1f)
+				{
+					movingDirection = MovingDirection.down;
+				}
+				if (rbody.velocity.y > -0.1f)
+					movingDirection = MovingDirection.up;
+
+				break;
+		}
+
+		switch (platformVaribles.groundState)
+		{
+			case GroundState.InAir:
+				movementState = MovementState.Jumping;
+				break;
+		}
+	}
 }
 
 
 #region Enums
 public enum MovementType
 {
-    Platformer,
-    TopDown
+	Platformer,
+	TopDown
 }
 
 public enum MovementState
 {
-    Idle,
-    Moving,
-    Jumping
+	Idle,
+	Moving,
+	Jumping
 }
 
 public enum GroundState
 {
-    InAir,
-    onGround
+	InAir,
+	onGround
 }
 
 public enum MovingDirection
 {
-    Left,
-    Right,
-    up,
-    down
+	Left,
+	Right,
+	up,
+	down
 }
 #endregion
 
 [System.Serializable]
 public class PlatformVars
 {
-    [Space]
-    [Header("States")]
-    [Space]
-    [Header("Remember to have a ground trigger collider!")]
-    public GroundState groundState;
+	[Space]
+	[Header("States")]
+	[Space]
+	[Header("Remember to have a ground trigger collider!")]
+	public GroundState groundState;
 
-    [Header("Movement")]
-    [Space]
-    public float Speed;
-    public bool UseCustomGravity;
-    public bool FlipSpriteOnMovement;
+	[Header("Movement")]
+	[Space]
+	public float Speed;
+	public bool UseCustomGravity;
+	public bool FlipSpriteOnMovement;
 
-    [Header("Jump values")]
-    public float JumpForce;
-    public bool UseJumpRotation;
-    public float JumpRotation;
-    public float JumpRotationMuliplyer;
-    public bool snaprotation;
-    public float SmoothRotation;
+	[Header("Jump values")]
+	public float JumpForce;
+	public bool UseJumpRotation;
+	public float JumpRotation;
+	public float JumpRotationMuliplyer;
+	public bool snaprotation;
+	public float SmoothRotation;
 
-    [Header("double jump")]
-    [Space]
-    public bool UseDoubleJump = true;
-    public int doubleJumps;
-    public int AirJumpsLeft;
+	[Header("double jump")]
+	[Space]
+	public bool UseDoubleJump = true;
+	public int doubleJumps;
+	public int AirJumpsLeft;
 
-    [Header("Sounds")]
-    [Space]
-    public AudioClip JumpSound;
-    public AudioClip walkingSound;
+	[Header("Sounds")]
+	[Space]
+	public AudioClip JumpSound;
+	public AudioClip walkingSound;
 
-    [Header("Sprites")]
-    [Space]
-    public Sprite WalkingSprite;
-    public Sprite JumpSprite;
+	[Header("Sprites")]
+	[Space]
+	public Sprite WalkingSprite;
+	public Sprite JumpSprite;
 
-    [HideInInspector]
-    public int Jumps;
+	[HideInInspector]
+	public int Jumps;
 
-    [Header("string")]
-    [Space]
-    public string groundTag;
+	[Header("string")]
+	[Space]
+	public string groundTag;
 }
 
 [System.Serializable]
 public class TopDownVars
 {
-    [Header("Movement speed")]
-    public bool SmoothMovement = true;
-    [Space]
-    public bool SameSpeedOnXAndY;
-    public float XSpeed, YSpeed;
+	[Header("Movement speed")]
+	public bool SmoothMovement = true;
+	[Space]
+	public bool SameSpeedOnXAndY;
+	public float XSpeed, YSpeed;
 }
--- Assets/Script/Player/PlayerShooting.cs
+++ Assets/Script/Player/PlayerShooting.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +11,9 @@ public class PlayerShooting : MonoBehaviour
 
 	[Range(-1f, 1.5f)]
 	public float shootSpeed;
+	public float randangle = .1f;
+	public float randspeed = 3f;
+	public int spitballs = 10;
 
 	// Start is called before the first frame update
 	void Start()
@@ -26,8 +28,21 @@ public class PlayerShooting : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.Mouse0))
 		{
-			Instantiate(spitball, firePoint.position, Quaternion.LookRotation(lastAimDir, Vector3.back) * Quaternion.Euler(90f, 0f, 0f)).GetComponent<Spitball>().Initialize(Mathf.Pow(10, shootSpeed));
+			for (int i = 0; i < spitballs; i++)
+			{
+				float angle = Random.Range(-randangle, randangle);
+				float sin = Mathf.Sin(angle);
+				float cos = Mathf.Cos(angle);
+				float x = lastAimDir.x * cos - lastAimDir.y * sin;
+				float y = lastAimDir.x * sin + lastAimDir.y * cos;
+				Shoot(new Vector2(x, y), Mathf.Pow(10, shootSpeed) + Random.Range(-randspeed, randspeed));
+			}
 		}
+	}
+
+	private void Shoot(Vector2 dir, float speed)
+	{
+		Instantiate(spitball, firePoint.position, Quaternion.LookRotation(dir.normalized, Vector3.back) * Quaternion.Euler(90f, 0f, 0f)).GetComponent<Spitball>().Initialize(speed);
 	}
 
 	private Vector2 GetFireDir()
